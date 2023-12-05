925f85b0da5fcba8e86381a8e6d5a9ffa10b85ea
dgruending <dgruending23@gmail.com>
2023-12-05 05:58:15 +0100

    Added Argumentparser and fastq file reading.

--- fastqc.py
+++ fastqc.py
@@ -1,3 +1,14 @@
+from argparse import ArgumentParser
+from os import path
+import pyfastx
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser()
+    # TODO add help text
+    parser.add_argument("-f", "--file", dest="file_path", help="", required=True)
+    parser.add_argument("-o", "--output", dest="output", help="")
+
+    args = parser.parse_args()
+
+    if path.exists(args.file_path):
+        fq_file = pyfastx.Fastq(args.file_path)
