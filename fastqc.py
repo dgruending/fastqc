ba66bac9c4fffac1745102384884f5edba30f48d
dgruending <dgruending23@gmail.com>
2023-12-10 09:25:24 +0100

    Added aggregate function to get data for statistics. Save all quality scores for "per base sequence quality".

--- fastqc.py
+++ fastqc.py
@@ -1,6 +1,19 @@
 from argparse import ArgumentParser
 from os import path
+
+import numpy as np
 import pyfastx
+
+
+def aggregate(fastq, quality_scores, start_index=0, end_index=0):
+    """
+
+    :return:
+    """
+    for ind in range(start_index, end_index+1):
+        seq = fastq[ind].seq
+        qualities = fastq[ind].quali
+        quality_scores[ind, 0:len(qualities)] = qualities
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -12,3 +25,7 @@ if __name__ == '__main__':
 
     if path.exists(args.file_path):
         fq_file = pyfastx.Fastq(args.file_path)
+        qual_scores = np.zeros((len(fq_file), fq_file.maxlen))
+        aggregate(fq_file, qual_scores, start_index=0, end_index=len(fq_file)-1)
+    else:
+        raise FileNotFoundError(f"FastQ file \"{args.file_path}\" does not exist.")
