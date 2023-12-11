ecd51548fe4126aaa2182692c603830662a4015a
dgruending <dgruending23@gmail.com>
2023-12-11 08:24:20 +0100

    File Reading is its own function. Init quality score array with nan values instead of zero.

--- fastqc.py
+++ fastqc.py
@@ -5,15 +5,27 @@ import numpy as np
 import pyfastx
 
 
+def read_fastq_file(filepath):
+    if path.exists(filepath):
+        return pyfastx.Fastq(args.file_path)
+    else:
+        raise FileNotFoundError(f"FastQ file \"{args.file_path}\" does not exist.")
+
+
 def aggregate(fastq, quality_scores, start_index=0, end_index=0):
     """
 
     :return:
     """
-    for ind in range(start_index, end_index+1):
+    for ind in range(start_index, end_index + 1):
         seq = fastq[ind].seq
         qualities = fastq[ind].quali
         quality_scores[ind, 0:len(qualities)] = qualities
+
+
+def qual_per_base(quali_arr):
+    pass
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -23,9 +35,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if path.exists(args.file_path):
-        fq_file = pyfastx.Fastq(args.file_path)
-        qual_scores = np.zeros((len(fq_file), fq_file.maxlen))
-        aggregate(fq_file, qual_scores, start_index=0, end_index=len(fq_file)-1)
-    else:
-        raise FileNotFoundError(f"FastQ file \"{args.file_path}\" does not exist.")
+    fq_file = read_fastq_file(args.file_path)
+
+    num_seqs = len(fq_file)
+
+    qual_scores = np.full((num_seqs, fq_file.maxlen), np.nan)
+    aggregate(fq_file, qual_scores, start_index=0, end_index=num_seqs - 1)
+
+    qual_per_base(qual_scores)
