1b9e2ad205b3a50630a7a917751cc662b159b170
dgruending <dgruending23@gmail.com>
2024-01-29 09:49:27 +0100

    no suffix tree generation, when there are no motifs

--- fastqc.py
+++ fastqc.py
@@ -363,11 +363,12 @@ def aggregate(file_path, max_len, motifs, block):
             base_content[Base[seq[base_ind]].value, base_ind] += 1
 
         # motif matching
-        tree = Tree({0: seq})
-        for motif_ind, motif in enumerate(motifs):
-            motif_start = find_index(tree, motif)
-            if motif_start < len(seq):
-                motifs_occ[motif_ind, motif_start] += 1
+        if motifs:
+            tree = Tree({0: seq})
+            for motif_ind, motif in enumerate(motifs):
+                motif_start = find_index(tree, motif)
+                if motif_start < len(seq):
+                    motifs_occ[motif_ind, motif_start] += 1
 
     return quality_scores, lengths, base_content, motifs_occ
 
